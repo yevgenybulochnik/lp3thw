f63b57fc783b8ee82a7cc7d93e31ddfb4fc5db41
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-10-31 22:22:14 -0700

    Exercise 1 drills

---
+++ ex1/drill1.py
@@ -0,0 +1,11 @@
+# Make your script print another line
+
+print("Hello world!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+print("I am printing another line")
---
+++ ex1/drill3.py
@@ -0,0 +1,11 @@
+# Use the octothorpe character and explain what it does 
+
+print("Hello world!")
+# print("Hello Again")
+print("I like typing this.")
+# print("This is fun.")
+print('Yay! Printing.')
+# print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# character is used to comment out lines of code
