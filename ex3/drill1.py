8b05f74e64aec84139e0c3b47cb2d9244af8cf6d
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-11-06 20:26:45 -0800

    Exercise 3 drill 1

---
+++ ex3/drill1.py
@@ -0,0 +1,33 @@
+# Simple print statement that prints a string
+print("I will count my chickens:")
+
+# Prints a string then divides 30 by 6 then adds 25
+print("Hens", 25 + 30 / 6)
+# Prints a string then gives you the remainder of 75/3 or 3 and subtracts from 100
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+# 4/2 remainder = 0, total adds up to 6.75
+print(3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
+# Prints a string
+print("Is it true that 3 + 2 < 5 - 7?")
+# false statement
+print(3 + 2 < 5 - 7)
+
+# string plus result of 3 add 2
+print("What is 3 + 2?", 3 + 2)
+# string plus result of 5 subtract 7
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+
+print("How about some more.")
+
+# Returns true
+print("Is it greater?", 5 > -2)
+# Returns true
+print("Is it greater or equal?", 5 >= -2)
+# Returns false
+print("Is it less or equal?", 5 <= -2)
