75adbc4afeee4c3edb43c504c45bfdf675be7bc2
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-01-04 20:14:43 -0800

    Removed extra space

--- ex23/ex23.py
+++ ex23/ex23.py
@@ -12,7 +12,7 @@ def main(language_file, encoding, errors):
 
 def print_line(line, encoding, errors):
     next_lang = line.strip()
-    raw_bytes = next_lang.encode(encoding, errors =errors)
+    raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors=errors)
 
     print(raw_bytes, "<===>", cooked_string)
